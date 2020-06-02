4cfbdecacebbe17d7f15adff1081d9aad2a9b0e9
saresend <saresend@usc.edu>
2020-06-02 11:51:53 -0700

    now does random data interpolation too

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import * as d3 from "d3";
 import "./App.css";
+import { scaleLinear } from "d3-scale";
 import styled from "styled-components";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 
@@ -13,13 +14,24 @@ function App() {
     .scale([450])
     .translate([515, 550])
     .precision([0.1]);
+  const colorScale = scaleLinear()
+    .domain([0, 100])
+    .range(["#ffedea", "#ff5233"]);
   return (
     <ComposableMap projection={projection}>
       <Geographies geography={geoURL}>
         {({ geographies }) =>
-          geographies.map((geo) => (
-            <Geography key={geo.rsmKey} geography={geo} />
-          ))
+          geographies.map((geo) => {
+            console.log(geo);
+            const data = Math.random() * 100;
+            return (
+              <Geography
+                fill={colorScale([data])}
+                key={geo.rsmKey}
+                geography={geo}
+              />
+            );
+          })
         }
       </Geographies>
     </ComposableMap>
